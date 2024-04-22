b892d0592c90ea1ce8259fb623bca1111c022d36
nikhil <nikhilarroju47@gmail.com>
2024-04-22 14:31:17 +0530

    refactor

--- app/main.js
+++ app/main.js
@@ -13,6 +13,39 @@ function parseInput(data) {
   return arr;
 }
 
+function generateResponse(
+  statusCode,
+  content = "",
+  contentType = "text/plain"
+) {
+  let status = "OK";
+  if (statusCode === 404) {
+    status = "Not Found";
+    return `HTTP/1.1 ${statusCode} ${status}\nContent-Length: ${content.length}\r\n\r\n`;
+  }
+  if (content) {
+    return `HTTP/1.1 ${statusCode} ${status}\nContent-Type: ${contentType}\nContent-Length: ${content.length}\n\n${content}\r\n\r\n`;
+  } else {
+    return `HTTP/1.1 ${statusCode} ${status}\r\n\r\n`;
+  }
+}
+
+function generate200Response(content) {
+  if (content) {
+    return `HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ${content.length}\n\n${content}\r\n\r\n`;
+  } else {
+    return `HTTP/1.1 200 OK\r\n\r\n`;
+  }
+}
+
+function generate201Response() {
+  return `HTTP/1.1 201 OK\r\n\r\n`;
+}
+
+function generate404Response(content = "") {
+  return `HTTP/1.1 404 Not Found\r\nContent-Length: ${content.length}\r\n\r\n`;
+}
+
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
@@ -32,17 +65,11 @@ const server = net.createServer((socket) => {
     let userAgent = thirdLineData[1];
 
     if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      socket.write(generateResponse(200));
     } else if (path === "/user-agent") {
-      socket.write(
-        `HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ${userAgent.length}\n\n${userAgent}\r\n\r\n`
-      );
+      socket.write(generateResponse(200, userAgent));
     } else if (path.startsWith("/echo/")) {
-      socket.write(
-        `HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ${
-          path.slice(6).length
-        }\n\n${path.slice(6)}\r\n\r\n`
-      );
+      socket.write(generateResponse(200, path.slice(6)));
     } else if (path.startsWith("/files/")) {
       let filename = path.slice(7);
       if (requestType === "GET") {
@@ -52,17 +79,17 @@ const server = net.createServer((socket) => {
             "utf-8"
           );
           socket.write(
-            `HTTP/1.1 200 OK\nContent-Type: application/octet-stream\nContent-Length: ${fileContent.length}\n\n${fileContent}\r\n\r\n`
+            generateResponse(200, fileContent, "application/octet-stream")
           );
         } catch (error) {
-          socket.write("HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+          socket.write(generateResponse(404, ""));
         }
       } else if (requestType === "POST") {
         fs.writeFileSync(`${directory}/${filename}`, body);
-        socket.write(`HTTP/1.1 201 OK\r\n\r\n`);
+        socket.write(generateResponse(201, ""));
       }
     } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.write(generateResponse(404));
     }
   });
 });
